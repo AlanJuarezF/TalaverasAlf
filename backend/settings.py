648921542cf3e1236a625424361061140ae41a58
Alan Juarez <alaanjuarezf@gmail.com>
2024-02-10 18:05:34 -0600

    config host allowed

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['http://localhost:5173', 'talaverasalf-production.up.railway.app']
 
 
 # Application definition
